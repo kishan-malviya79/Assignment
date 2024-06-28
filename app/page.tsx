ebe3791478d0d11ccf2eefcdbf8716cdebdd42e7
kishan0779 <124876418+kishan0779@users.noreply.github.com>
2024-06-28 19:07:00 +0530

    s

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "./HookForm/LoginForm/LoginForm";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between sm:pt-0 pt-20 ">
+    <main className="flex min-h-screen flex-col items-center justify-center sm:pt-0 pt-8  ">
      <LoginForm/>
     </main>
   );
